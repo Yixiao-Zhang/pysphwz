7405088ba81b6e389942c5f49291c16447fbae45
Yixiao Zhang <yixiaozhang@pku.edu.cn>
2020-12-04 19:18:59 +0800

    Modify interactions between particles
    We follow the scheme that Ihmsen (2014) referred to as Algorithm 1.
    But there are problems in this code, which may be caused the
    choice of parameters or bugs in the code.

--- main.py
+++ main.py
@@ -9,12 +9,29 @@ import math
 import numpy as np
 import tinyarray as ta
 
-domain = ta.array([32.0, 18.0])
+domain = ta.array([16.0, 9.0])
 mid_domain = domain/2
 pradius = 1.0
+npoint = 576
+density0 = npoint/math.exp(sum(map(math.log, domain)))
+stiffness = 10.0
 
 def norm(array):
     return math.sqrt(sum(array*array))
+
+def weight(displacement):
+    dist = norm(displacement)
+    return max((2/math.pi)*(1 - dist**2), 0.0)
+
+def grad_weight(displacement):
+    dist = norm(displacement)
+    return (4/math.pi)*displacement if dist < 1.0 else 0.0
+
+def laplace(displacement):
+    return sum(displacement*grad_weight(displacement))/(norm(displacement)**2 + 1.0e-2)
+
+def pressure(density):
+    return stiffness*((density/density0)**7 - 1)
 
 class Point:
     def __init__(self, position, velocity):
@@ -41,20 +58,22 @@ class SFH:
         self.t = t0
 
     def step(self, dt=None):
+        vmax = max(norm(point.velocity) for point in self.points)
         if dt is None:
-            vmax = max(norm(point.velocity) for point in self.points)
             dt = 0.4*pradius/max(vmax, 1.0)
 
-        for point in self.points:
-            point.move(dt)
+        print('CFL', vmax*dt/pradius)
 
         for point in self.points:
             point.accelarate(dt, self)
 
+        for point in self.points:
+            point.move(dt)
+
         self.t += dt
 
 class SFH_APP(SFH, tk.Canvas):
-    scale = 32.0
+    scale = 50.0
     radius = 4.0
     def __init__(self, points):
         SFH.__init__(self, points)
@@ -64,7 +83,7 @@ class SFH_APP(SFH, tk.Canvas):
             width=domain[0]*self.scale, height=domain[1]*self.scale)
         self.ovals = self.plot_points()
         self.timer = self.create_text(20, 20, anchor=tk.NW,
-            font="Courier 10 bold",
+            font="Courier 20 bold",
             text="")
         self.pack()
 
@@ -76,16 +95,20 @@ class SFH_APP(SFH, tk.Canvas):
     def plot_points(self):
         ovals = list()
         for point in self.points:
+            try:
+                color = point.color
+            except:
+                color = '#000000'
             oval = self.create_oval(*self.coords_oval(point),
-                fill='#476042')
+                fill=color)
             ovals.append(oval)
         return tuple(ovals)
 
-    real_time_sep = 0.05
+    real_time_sep = 0.2
     def loop_func(self):
         time_next = time.time()
         while True:
-            self.step(dt=0.05)
+            self.step(dt=0.01)
             for point, oval in zip(self.points, self.ovals):
                 self.coords(oval, *self.coords_oval(point))
             self.itemconfig(self.timer, text = f't = {self.t:.02f}')
@@ -105,24 +128,29 @@ class LiquidApp(SFH_APP):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.update_subdomains()
+        self.density = density0 # dummy value
+        self.pressure = 0.0 # dummy value
 
     class LiquidPoint(Point):
         def force(self, model):
             res = ta.array([0.0, 0.0])
-            for point in model.neiborghs(self):
-                displacement = self.displacement(point)
-                distance = norm(displacement)
-                if distance < pradius:
-                    res += +0.5*displacement/max(distance, 0.1)**3
+            res += -model.gradient('pressure', self)
+            res += 0.2*model.laplace('velocity', self)
+            res += (self.position - domain/2)*ta.array([0.0, -1.0])
             return res
 
+        @property
+        def color(self):
+            return '#FF0000' if self.position[1] > domain[1]/2 else '#FFFF00'
+
     def step(self, *args, **kwargs):
-        super().step(*args, **kwargs)
         self.update_subdomains()
+        self.update_density()
+        super().step(*args, **kwargs)
 
     # make sure that the subdomain is large enough to capture
     # partical-partical interactions
-    nsubs = (32, 18)
+    nsubs = (16, 9)
     def update_subdomains(self):
         self.subs = [[set() for _ in range(nsub)] for nsub in self.nsubs]
         for point in self.points:
@@ -136,6 +164,12 @@ class LiquidApp(SFH_APP):
                         for k in (-1, 0, 1))))
             self._neiborghs[indexes] = set.intersection(*sets)
 
+    def update_density(self):
+        for point in self.points:
+            point.density = sum(weight(point.position - neighbor.position)
+                for neighbor in self.neiborghs(point))
+            point.pressure = pressure(point.density)
+
     def subdomain_index(self, point):
         return (int(point.position[i]*self.nsubs[i]/domain[i])
                     for i in range(len(self.nsubs)))
@@ -143,15 +177,29 @@ class LiquidApp(SFH_APP):
     def neiborghs(self, point):
         return self._neiborghs[tuple(self.subdomain_index(point))]
 
+    def gradient(self, vname, point):
+        return sum(
+               (getattr(point, vname)/point.density**2
+               + getattr(neighbor, vname)/neighbor.density**2)
+                * grad_weight(neighbor.position - point.position)
+                for neighbor in self.neiborghs(point)
+        )
+
+    def laplace(self, vname, point):
+        return -2*sum(
+               getattr(point, vname)/neighbor.density
+                * laplace(neighbor.position - point.position)
+                for neighbor in self.neiborghs(point)
+        )
+
 
 def main():
-    npoint = 576
     xs = np.random.uniform(0.0, domain[0], npoint)
     ys = np.random.uniform(0.0, domain[1], npoint)
     positions = map(ta.array, map(list, zip(xs, ys)))
 
-    vxs = np.random.uniform(1.0, 1.0, npoint)
-    vys = np.random.uniform(0.0, 0.0, npoint)
+    vxs = np.random.uniform(-1.0, 1.0, npoint)
+    vys = np.random.uniform(-1.0, 1.0, npoint)
     velocities = map(ta.array, map(list, zip(vxs, vys)))
 
     points = tuple(map(LiquidApp.LiquidPoint, positions, velocities))
