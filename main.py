0711bf1264079e9803aa3c975cd4fad2f10ac277
Yixiao Zhang <yixiaozhang@pku.edu.cn>
2020-12-21 16:31:23 +0800

    Clean up

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ import functools
 import numpy as np
 import tinyarray as ta
 
-domain = ta.array([16.0, 8.0])
+domain = ta.array([16.0, 16.0])
 mid_domain = domain/2
 nx = 24
-ny = 12
+ny = 24
 npoint = nx*ny
 density0 = npoint/math.exp(sum(map(math.log, domain)))
 stiffness = 1.0e3
-viscosity = 1.0e-1
+viscosity = 0.0
+nudge = 0.0
 k = 7
 pradius = 1.0
 
@@ -64,15 +65,6 @@ def grad_weight(displacement):
         )
     return ta.array(res)
 
-def laplace(displacement):
-    return (
-                sum(displacement*grad_weight(displacement))
-                / (sum(displacement*displacement) + pradius**2*1.0e-2)
-            )
-
-def pressure(density):
-    return stiffness*((density/density0)**k - 1)
-    # return stiffness*(density/density0 - 1)
 
 class Point:
     def __init__(self, position, velocity):
@@ -93,21 +85,57 @@ class Point:
     def force(self, model):
         return 0.0
 
-class SFH:
-    def __init__(self, points, t0=0.0):
+
+class NS:
+    def __init__(self, points):
         self.points = points
+        self.nsubs = tuple(map(math.ceil, domain/pradius))
+
+    def update_ns(self):
+        self.subs = [[set() for _ in range(nsub)] for nsub in self.nsubs]
+        for point in self.points:
+            for i, subi in enumerate(self.subdomain_index(point)):
+                self.subs[i][subi].add(point)
+        self._neiborghs = dict()
+        for indexes in itertools.product(*map(range, self.nsubs)):
+            sets = []
+            for i, subi in enumerate(indexes):
+                sets.append(set.union(*(self.subs[i][(subi+k)%self.nsubs[i]]
+                        for k in (-1, 0, 1))))
+            self._neiborghs[indexes] = set.intersection(*sets)
+
+        for point in self.points:
+            point.density = sum(weight(neighbor.displacement(point))
+                for neighbor in self.neiborghs(point))
+
+    def subdomain_index(self, point):
+        return (int(point.position[i]*self.nsubs[i]/domain[i])%self.nsubs[i]
+                    for i in range(len(self.nsubs)))
+
+    def neiborghs(self, point):
+        return self._neiborghs[tuple(self.subdomain_index(point))]
+
+
+class SPH(NS):
+    def __init__(self, points, t0=0.0):
+        super().__init__(points)
         self.t = t0
+        self.nstep = 0
 
     def step(self, dt=None):
+
+        self.update_ns()
+
         vmax = max(norm(point.velocity) for point in self.points)
         rho_max = max(point.density for point in self.points)
         rho_min = min(point.density for point in self.points)
         if dt is None:
-            dt = 0.4*pradius/max(vmax, 1.0)
+            dt = 0.4*pradius/max(ca, 1.0)
 
-        print('CFL', vmax*dt/pradius)
-        print('Mach number', vmax/ca)
-        print('Density {0:.3f} {1:.3f}'.format(rho_min, rho_max))
+        if self.nstep % 10 == 0:
+            print('CFL', vmax*dt/pradius)
+            print('Mach number', vmax/ca)
+            print('Density {0:.3f} {1:.3f}'.format(rho_min, rho_max))
 
         for point in self.points:
             point.accelarate(dt, self)
@@ -116,12 +144,37 @@ class SFH:
             point.move(dt)
 
         self.t += dt
+        self.nstep += 1
 
-class SFH_APP(SFH, tk.Canvas):
+    def gradient(self, vname, point):
+        return sum(
+               (getattr(point, vname)/point.density**2
+               + getattr(neighbor, vname)/neighbor.density**2)
+                * grad_weight(neighbor.displacement(point))
+                for neighbor in self.neiborghs(point) if neighbor != point
+        )
+
+    @staticmethod
+    def laplace_marco(displacement):
+        return (
+                    sum(displacement*grad_weight(displacement))
+                    / (sum(displacement*displacement) + pradius**2*1.0e-2)
+                )
+
+    def laplace(self, vname, point):
+        return 2*sum(
+               (getattr(neighbor, vname)- getattr(point, vname))
+                * self.laplace_marco(neighbor.displacement(point))
+                /neighbor.density
+                for neighbor in self.neiborghs(point) if neighbor != point
+        )
+
+
+class SPH_APP(SPH, tk.Canvas):
     scale = 50.0
     radius = 4.0
     def __init__(self, points):
-        SFH.__init__(self, points)
+        SPH.__init__(self, points)
 
         self.root = tk.Tk()
         tk.Canvas.__init__(self, self.root,
@@ -153,7 +206,7 @@ class SFH_APP(SFH, tk.Canvas):
     def loop_func(self):
         time_next = time.time()
         while True:
-            self.step(dt=0.01)
+            self.step(dt=0.005)
             for point, oval in zip(self.points, self.ovals):
                 self.coords(oval, *self.coords_oval(point))
             self.itemconfig(self.timer, text = f't = {self.t:.02f}')
@@ -168,90 +221,44 @@ class SFH_APP(SFH, tk.Canvas):
         self.root.after(0, self.loop_func)
         tk.mainloop()
 
-class LiquidApp(SFH_APP):
+class LiquidPoint(Point):
+    def force(self, model):
+        res = ta.array([0.0, 0.0])
+        res += -model.gradient('pressure', self)
+        if viscosity:
+            res += viscosity*model.laplace('velocity', self)
+        if nudge:
+            res -= self.velocity*nudge
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.update_subdomains()
-        self.density = density0 # dummy value
-        self.pressure = 0.0 # dummy value
+        res += ta.array([0.0, 10.0 if self.position[1] < mid_domain[1] else -10.0])
+        if model.t < 1.0:
+            res = res*ta.array([0.0, 1.0])
+        return res
 
-    class LiquidPoint(Point):
-        def force(self, model):
-            res = ta.array([0.0, 0.0])
-            res += -model.gradient('pressure', self)
-            if viscosity:
-                res += viscosity*model.laplace('velocity', self)
-            return res
+    @property
+    def color(self):
+        return '#FF0000' if self.position[1] > domain[1]/2 else '#FFFF00'
 
-        @property
-        def color(self):
-            return '#FF0000' if self.position[1] > domain[1]/2 else '#FFFF00'
-
-    def step(self, *args, **kwargs):
-        self.update_subdomains()
-        self.update_density()
-        super().step(*args, **kwargs)
-
-    # make sure that the subdomain is large enough to capture
-    # partical-partical interactions
-    nsubs = tuple(map(math.ceil, domain/pradius))
-    def update_subdomains(self):
-        self.subs = [[set() for _ in range(nsub)] for nsub in self.nsubs]
-        for point in self.points:
-            for i, subi in enumerate(self.subdomain_index(point)):
-                self.subs[i][subi].add(point)
-        self._neiborghs = dict()
-        for indexes in itertools.product(*map(range, self.nsubs)):
-            sets = []
-            for i, subi in enumerate(indexes):
-                sets.append(set.union(*(self.subs[i][(subi+k)%self.nsubs[i]]
-                        for k in (-1, 0, 1))))
-            self._neiborghs[indexes] = set.intersection(*sets)
-
-    def update_density(self):
-        for point in self.points:
-            point.density = sum(weight(neighbor.displacement(point))
-                for neighbor in self.neiborghs(point))
-            point.pressure = pressure(point.density)
-
-    def subdomain_index(self, point):
-        return (int(point.position[i]*self.nsubs[i]/domain[i])%self.nsubs[i]
-                    for i in range(len(self.nsubs)))
-
-    def neiborghs(self, point):
-        return self._neiborghs[tuple(self.subdomain_index(point))]
-
-    def gradient(self, vname, point):
-        return sum(
-               (getattr(point, vname)/point.density**2
-               + getattr(neighbor, vname)/neighbor.density**2)
-                * grad_weight(neighbor.displacement(point))
-                for neighbor in self.neiborghs(point) if neighbor != point
-        )
-
-    def laplace(self, vname, point):
-        return 2*sum(
-               (getattr(neighbor, vname)- getattr(point, vname))/neighbor.density
-                * laplace(neighbor.displacement(point))
-                for neighbor in self.neiborghs(point) if neighbor != point
-        )
+    @property
+    def pressure(self):
+        return stiffness*((self.density/density0)**k - 1)
 
 
 def main():
     xs = np.linspace(0.0, domain[0], nx, endpoint=False)
     ys = np.linspace(0.0, domain[1], ny, endpoint=False)
     xs, ys = map(lambda a: np.reshape(a, (-1, )),np.meshgrid(xs, ys))
-    # xs, ys = map(lambda a: a[:len(a)//2], (xs, ys))
+    preserved = np.where((np.abs((xs-mid_domain[0])/domain[0]) < 0.2)*(np.abs((ys-mid_domain[1])/domain[1]) < 0.4))[0]
+    xs, ys = map(lambda a: a[preserved], (xs, ys))
     positions = map(ta.array, map(list, zip(xs, ys)))
 
-    vxs = 10.0*(2*(ys > mid_domain[1]) - 1)
+    vxs = 00.0*(2*(ys > mid_domain[1]) - 1)
     vys = np.random.uniform(-1.0e-3, 1.0e-3, len(xs))
     velocities = map(ta.array, map(list, zip(vxs, vys)))
 
-    points = tuple(map(LiquidApp.LiquidPoint, positions, velocities))
+    points = tuple(map(LiquidPoint, positions, velocities))
 
-    model = LiquidApp(points)
+    model = SPH_APP(points)
     model.run()
 
 
